d696d049738a7da2e21c24df84b292afa14d3e54
Uriel Buciio <urielte.cruzsa@gmail.com>
2022-05-09 18:41:03 -0500

    Scope JS

---
+++ 02-IntroJS/07-Scope/js/main.js
@@ -0,0 +1,13 @@
+var color = 'negro';
+
+function pinturas() {
+    var color = 'rojo';
+    console.log(color);
+
+    var listaColores = `Mi color favorito es: ${color}`;
+    console.log(listaColores);
+}
+
+pinturas(color);
+
+console.log(color)
