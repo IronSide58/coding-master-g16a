d247578455322763dd994d59554b4a5bc8b18f21
Uriel Buciio <urielte.cruzsa@gmail.com>
2022-06-13 12:58:31 -0500

    Ejercicios Colas

---
+++ 03-ComputerScience/06-Colas/js/main.js
@@ -0,0 +1,73 @@
+class Queue{
+    constructor(){
+        this.items = [];
+    }
+    enqueue(item) {
+        this.items.push(item);
+    }
+    dequeue() {
+        return this.items.shift();
+    }
+    recorrerqueue(index){
+        return this.items[index];
+    }
+    peek() {
+        return this.items[0];
+    }
+    size() {
+        return this.items.length;
+    }
+    print() {
+        return this.items;
+    }
+}
+
+/*Ejercicio 1. Se tiene una cola de colores y se tiene que dividir en dos colas, respetando el orden y alternando a partir de su índice. los pares en una y los nones en otra */
+let queue_color = new Queue();
+queue_color.enqueue('amarillo')
+queue_color.enqueue('rosa')
+queue_color.enqueue('rojo')
+queue_color.enqueue('verde')
+queue_color.enqueue('azul')
+queue_color.enqueue('negro')
+queue_color.enqueue('morado')
+queue_color.enqueue('blanco')
+
+function separarQueues(queue) {
+    let queue_par = new Queue();
+    let queue_impar = new Queue();
+    for (let i = 0; i < queue.size(); i++) {
+        if (i %2 == 0) {
+            queue_par.enqueue(queue.recorrerqueue(i));
+        } else {
+            queue_impar.enqueue(queue.recorrerqueue(i));
+        }
+    }
+    return `Cola 1: ${queue_par.print()} 
+    Cola 2: ${queue_impar.print()}`;
+}
+
+/*Ejercicio 2.- Se tiene una cola en la cual se han repartido tickets con el orden de atención. Sin embargo, llegada la hora de inicio hay muchos “colados”, es por esto que se le ordena al vigilante que retire a todos aquellos que no tienen ticket. Muestra la cola inicial, qué elementos fueron retirados de la cola y la cola final. */
+let queue_tickets = new Queue();
+queue_tickets.enqueue({user: 'User 1', ticket: true})
+queue_tickets.enqueue({user: 'User 2', ticket: true})
+queue_tickets.enqueue({user: 'User 3', ticket: false})
+queue_tickets.enqueue({user: 'User 4', ticket: true})
+queue_tickets.enqueue({user: 'User 5', ticket: false})
+queue_tickets.enqueue({user: 'User 6', ticket: false})
+queue_tickets.enqueue({user: 'User 7', ticket: true})
+queue_tickets.enqueue({user: 'User 8', ticket: true})
+queue_tickets.enqueue({user: 'User 9', ticket: true})
+queue_tickets.enqueue({user: 'User 10', ticket: false})
+queue_tickets.enqueue({user: 'User 11', ticket: true})
+
+function sinColados(queue) {
+    let queueSinColados = new Queue();
+    var size = queue.size();
+    for (let i = 0; i < size; i++) {
+        if(queue.recorrerqueue(i).ticket == true){
+            queueSinColados.enqueue(queue.recorrerqueue(i));
+        }
+    }
+    return queueSinColados.print();
+}
