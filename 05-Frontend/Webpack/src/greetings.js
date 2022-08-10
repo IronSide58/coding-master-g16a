5d331d2fb1f603d324bbb0bb32171b494ebfa8d4
Uriel Buciio <urielte.cruzsa@gmail.com>
2022-08-09 21:32:33 -0500

    File

---
+++ 05-Frontend/Webpack/src/greetings.js
@@ -0,0 +1,17 @@
+'use strict';
+// function greetings(name) {
+//     return `Hola ${name} desde Webpack con JS`;
+// }
+
+// // Arrow function
+
+// const greetings = (name) => {
+//     return `Hola ${name} desde Webpack con JS`;
+// }
+
+const greetings = (name) => `Hola ${name} desde Webpack con JS`;
+
+module.exports = { greetings };
+
+// Esta es la forma que usa REACT y EMS6
+// export default { greetings };
---
+++ 05-Frontend/Webpack/src/main.js
@@ -0,0 +1,7 @@
+'use strict';
+const {greetings} = require("./greetings")
+require("./styles.css")
+require("./styles.scss")
+
+console.log('Mi primer proyecto Webpack')
+console.log(greetings('Uriel'))
