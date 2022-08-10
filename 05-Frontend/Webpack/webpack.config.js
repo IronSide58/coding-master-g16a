783b3e9676b2eec59c1c4b6d2f8ef193b5682b51
Uriel Buciio <urielte.cruzsa@gmail.com>
2022-08-09 21:30:31 -0500

    Loaders css, scss, plugin css

--- 05-Frontend/Webpack/webpack.config.js
+++ 05-Frontend/Webpack/webpack.config.js
@@ -1,10 +1,13 @@
 const HTMLWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 
 module.exports = {
-    entry: './src/js/main.js', //Permite cambiar la ruta de donde tomara el index.js o main.js,
+    entry: './src/main.js', //Permite cambiar la ruta de donde tomara el index.js o main.js,
     module: {
         rules: [
-            /*Aquí se definen LOADERS 
+            /*Aquí se definen LOADERS -> Reglas o instrucciones que nos ayudan a procesar
+            archivos para generar nuestra salida.
                 -> Sintaxis
                 {
                     * test: REGEX //Buscar archivos o extensiones en el proyecto
@@ -18,10 +21,20 @@ module.exports = {
                         options: { minimize: true }
                     }
                 ]
+            },
+            {
+                test: /.(css|scss)$/,
+                use: [
+                    'style-loader', //Procesa los estilos en linea
+                    'css-loader', //Procesa los estilos en archivos
+                    'sass-loader', //Procesa los estilos en archivos sass
+                ]
             }
         ]
     },
     plugins: [
-        new HTMLWebPackPlugin({ template: './src/index.html'})
+        /** Agregan funcionalidades extra. */
+        new HTMLWebPackPlugin({ template: './src/index.html'}),
+        new MiniCssExtractPlugin()
     ]
 }
