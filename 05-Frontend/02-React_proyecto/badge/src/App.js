50e7e19fc9e6ea8aafb2a90eb1dedeb527c6358a
Uriel Buciio <urielte.cruzsa@gmail.com>
2022-08-15 21:55:42 -0500

    Petición a API, creación de hijos

--- 05-Frontend/02-React_proyecto/badge/src/App.js
+++ 05-Frontend/02-React_proyecto/badge/src/App.js
@@ -1,14 +1,71 @@
 import React from 'react';
 import NavBar from './components/navBar';
 import Badge from './components/badge';
+import BadgeForm from './components/badgeForm';
+import CharacterItem from './components/CharacterItem';
+import axios from 'axios';
 
-function App() {
-  return (
-    <>
-      <NavBar/> 
-      <Badge firstName="Uriel" lastName="Bucio" jobTitle="Frontend" userName="UrielBucio"/>
-    </>
-  );
+class App extends React.Component {
+  state = {
+    character: [],
+    form: {
+      firstName: '',
+      lastName: '',
+      jobTitle: '',
+      userName: '',
+    }
+  };
+  componentDidMount() {
+    this.fetchApi();
+  }
+  componentDidUpdate() {
+
+  }
+  componentWillUnmount() {
+
+  }
+  fetchApi = () => {
+    axios.get("https://rickandmortyapi.com/api/character")
+    .then(res => {
+      const character = res.data.results;
+      
+      this.setState({
+        ...this.state,
+        character
+      })
+    })
+  };
+  handleChange = e => {
+    this.setState({ 
+      form: {
+        ...this.state.form,
+        [e.target.name]: e.target.value
+      } 
+    });
+  }
+  render() {
+    return (
+      <>
+        <NavBar/> 
+        <BadgeForm handleChange = {this.handleChange}/>
+        <Badge state={this.state}/>
+        <div className="container">
+          <div className="row align-items-start">
+            {/* <ul className="row"> */}
+              {/* <li className="col-6 col-md-3"> */}
+                {
+                  this.state.character.map(character => (
+                    <div className='col-md-3'>
+                      <CharacterItem character = {character} key = {character.id} />
+                    </div>))
+                }
+              {/* </li>
+            </ul> */}
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
