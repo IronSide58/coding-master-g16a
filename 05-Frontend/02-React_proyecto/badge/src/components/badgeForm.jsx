7206968f78f548e4dc8a83f881481b2910fa56d2
Uriel Buciio <urielte.cruzsa@gmail.com>
2022-08-15 21:54:42 -0500

    Creación de las inputs, y states

--- 05-Frontend/02-React_proyecto/badge/src/components/badgeForm.jsx
+++ 05-Frontend/02-React_proyecto/badge/src/components/badgeForm.jsx
@@ -1,1 +1,32 @@
 import React from "react";
+
+class BadgeForm extends React.Component {
+    render() {
+        return (
+            <div className="col Badge">
+                <h1>Crear Card</h1>
+                <form>
+                    <div className="form-group">
+                        <label>Primer Nombre</label>
+                        <input onChange={this.props.handleChange} type="text" className="form-control" name="firstName"/>
+                    </div>
+                    <div className="form-group">
+                        <label>Apellido</label>
+                        <input onChange={this.props.handleChange} type="text" className="form-control" name="lastName"/>
+                    </div>
+                    <div className="form-group">
+                        <label>Ocupación</label>
+                        <input onChange={this.props.handleChange} type="text" className="form-control" name="jobTitle"/>
+                    </div>
+                    <div className="form-group">
+                        <label>Usuario</label>
+                        <input onChange={this.props.handleChange} type="text" className="form-control" name="userName"/>
+                    </div>
+                    <button type="submit" className="form-control btn btn-primary">Enviar</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default BadgeForm;
