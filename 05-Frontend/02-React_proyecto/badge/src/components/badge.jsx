46a0c62dedb9e0fd632d47dd8a71b7f951abd7af
Uriel Buciio <urielte.cruzsa@gmail.com>
2022-08-15 21:54:00 -0500

    Enviar datos entre componentes

--- 05-Frontend/02-React_proyecto/badge/src/components/badge.jsx
+++ 05-Frontend/02-React_proyecto/badge/src/components/badge.jsx
@@ -5,24 +5,26 @@ import logo from "../images/devf-white.png"
 class Badge extends React.Component {
     render() {
         return (
-            <div className="Badge">
-                <div className="Badge__header">
-                    <img src={logo} alt="Devf"></img>
-                </div>
-                <div className="Badge__section-name">
-                    <img src="https://www.gravatar.com/avatar?d=identicon
-                    " className="Badge__avatar" alt="avatar"></img>
-                    <h1>
-                        {this.props.firstName} <br/>
-                        {this.props.lastName}
-                    </h1>
-                </div>
-                <div className="Badge__section-info">
-                    <h3>{this.props.jobTitle}</h3>
-                    <div>@{this.props.userName}</div>
-                </div>
-                <div className="Badge__footer">
-                    #Devf
+            <div className="col">
+                <div className="Badge">
+                    <div className="Badge__header">
+                        <img src={logo} alt="Devf"></img>
+                    </div>
+                    <div className="Badge__section-name">
+                        <img src="https://www.gravatar.com/avatar?d=identicon
+                        " className="Badge__avatar" alt="avatar"></img>
+                        <h1>
+                            {this.props.state.form.firstName} <br/>
+                            {this.props.state.form.lastName}
+                        </h1>
+                    </div>
+                    <div className="Badge__section-info">
+                        <h3>{this.props.state.form.jobTitle}</h3>
+                        <div>@{this.props.state.form.userName}</div>
+                    </div>
+                    <div className="Badge__footer">
+                        #Devf
+                    </div>
                 </div>
             </div>
         )
