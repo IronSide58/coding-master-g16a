0fe641123e62f0c7e4b6f4666fbc8a6ea968b322
Uriel Buciio <urielte.cruzsa@gmail.com>
2022-08-23 13:55:40 -0500

    Folder Name

--- 05-Frontend/05/conditional-rendering/src/components/ActiveUser.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ActiveUser = () => {
-  return (
-    <h3>Active User</h3>
-  )
-}
-
-export default ActiveUser
--- 05-Frontend/05/conditional-rendering/src/components/Counter.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Counter = () => {
-  return (
-    <h3>El contador esta en 0</h3>
-  )
-}
-
-export default Counter
