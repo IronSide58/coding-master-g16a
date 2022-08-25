43a9e0f8afd5047b77c5359f967bf28d22ba2b47
Uriel Buciio <urielte.cruzsa@gmail.com>
2022-08-24 23:40:10 -0500

    Add components

--- 05-Frontend/Proyecto-Final/project_final/src/App.jsx
+++ 05-Frontend/Proyecto-Final/project_final/src/App.jsx
@@ -1,11 +1,15 @@
-import './App.css'
+import React from 'react';
+import './App.css';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
-  )
+    <>
+      <Header/>
+      <Home/>
+    </>
+  );
 }
 
-export default App
+export default App;
