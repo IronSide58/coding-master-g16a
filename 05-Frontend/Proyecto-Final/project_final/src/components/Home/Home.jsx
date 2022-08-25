e8fdabd02686228f027416751f2a6509e32fd488
Uriel Buciio <urielte.cruzsa@gmail.com>
2022-08-24 23:42:04 -0500

    Component home - Filter, get data and print data

---
+++ 05-Frontend/Proyecto-Final/project_final/src/components/Home/Home.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Card from '../Card/Card';
+
+const Home = () => {
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    getItems()
+  },[])
+
+  const getItems = async () => {
+    const data = await axios.get('https://ecomerce-master.herokuapp.com/api/v1/item')
+    .then(response => response.data)
+    .catch(err => console.error(err));
+    
+    const item = data.filter(item => item.image || item.images).map(item => (
+      {
+        id: item._id,
+        name: item.product_name,
+        image: item.image !== undefined ? item.image : item.images,
+        price: item.price
+      }
+    ))
+    setItems(item.filter(item => item.image.startsWith('http')))
+  }
+  return (
+    <main className="grid-container">
+      {
+        items.map(item => (
+          <Card key={item.id} {...item}/>
+        ))
+      }
+    </main>
+  )
+}
+
+export default Home
