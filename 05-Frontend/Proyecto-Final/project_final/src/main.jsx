60099fe3578427b069631aa9fa95257bc3519d73
Uriel Buciio <urielte.cruzsa@gmail.com>
2022-08-25 15:37:07 -0500

    React route details

--- 05-Frontend/Proyecto-Final/project_final/src/main.jsx
+++ 05-Frontend/Proyecto-Final/project_final/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
+import Details from './components/Details/Details';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -9,6 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<App />}/>
+        <Route path='/details/:id' element={<Details />}/>
         <Route path='*' element={<h1>404:  No found</h1>}/>
       </Routes>
     </BrowserRouter>
