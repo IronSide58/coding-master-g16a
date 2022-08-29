3afba69d61cbf7dabbaa81f57becc10bfa32c541
Uriel Buciio <urielte.cruzsa@gmail.com>
2022-08-29 12:50:44 -0500

    prop header

--- 05-Frontend/Proyecto-Final/project_final/src/App.jsx
+++ 05-Frontend/Proyecto-Final/project_final/src/App.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 
 function App() {
+  const [itemSearch, setItemSearch] = useState('');
+
+  const getId = (item) => {
+    setItemSearch(item);
+  }
+
   return (
     <>
-      <Header/>
-      <Home/>
+      <Header getId={getId}/>
+      <Home props={itemSearch} />
     </>
   );
 }
