dffcea262f3da04a610647329fe11d865370f7cd
Uriel Buciio <urielte.cruzsa@gmail.com>
2022-08-29 12:51:08 -0500

    details content

---
+++ 05-Frontend/Proyecto-Final/project_final/src/components/Details/Details.jsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect} from 'react'
+import axios from 'axios'
+import { useParams } from "react-router-dom";
+import Header from '../Header/Header';
+
+const Details = () => {
+  const [itemId, setItemId] = useState('')
+  let params = useParams()
+  
+  useEffect(() => {
+    (async () => {
+      try {
+        const data = await axios.get(`https://ecomerce-master.herokuapp.com/api/v1/item/${params.id}`);
+        setItemId(data.data);
+      } catch (error) {
+        console.error('Error', error);
+      }
+    })()
+  }, [])
+  
+  return (
+    <>
+      <Header />
+      <main>
+        <section className="details">
+          <div className="details-image">
+            <img src={itemId.image} alt={itemId.name}/>
+          </div>
+          <div className="details-content">
+            <h1>{itemId.product_name}</h1>
+            <h2>{itemId.brand}</h2>
+            <p>{itemId.description}</p>
+            <span className="category">{itemId.category}</span>
+            <strong>$ {itemId.price}</strong>
+            <div className="btn-shop">
+              <button type="submit" className="btn-shopping">Comprar</button>
+            </div>
+          </div>
+        </section>
+      </main>
+    </>
+  )
+}
+
+export default Details
