ad5454e50be1a46fe68826d8ea227045fff55c72
Uriel Buciio <urielte.cruzsa@gmail.com>
2022-08-30 19:51:49 -0500

    Rendering conditional routes

--- 05-Frontend/Proyecto-Final/project_final/src/App.jsx
+++ 05-Frontend/Proyecto-Final/project_final/src/App.jsx
@@ -1,19 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom'
 import './App.css';
-import Header from './components/Header/Header';
-import Home from './components/Home/Home';
+import Details from './components/Details/Details';
+import Login from './components/Login/Login';
+import SignUp from './components/SignUp/SignUp';
+import Home from './Home';
 
 function App() {
-  const [itemSearch, setItemSearch] = useState('');
+  const [isUserLogin, setIsUserLogin] = useState(false);
+  const [authentication, setAuthentication] = useState({});
+  console.log(isUserLogin);
 
-  const getId = (item) => {
-    setItemSearch(item);
-  }
+  // useEffect(() => {
+  //   const userSession =localStorage.getItem('isUserLogin');
+  //   setIsUserLogin(userSession);
+  // }, [])
+  
+  // useEffect(() => {
+  //   localStorage.setItem('isUserLogin', isUserLogin);
+  // }, [isUserLogin])
 
   return (
     <>
-      <Header getId={getId}/>
-      <Home props={itemSearch} />
+      <Routes>
+        <Route path='/' element={<Home isUserLogin={isUserLogin} authentication={authentication}/>}/>
+        {!isUserLogin && <>
+          <Route path='/login' element={<Login setAuthentication={setAuthentication} setIsUserLogin={setIsUserLogin}/>}/>
+          <Route path='/signup' element={<SignUp />}/>
+        </>}
+        <Route path='/details/:id' element={<Details />}/>
+        <Route path='*' element={<h1>404:  No found</h1>}/>
+      </Routes>
     </>
   );
 }
