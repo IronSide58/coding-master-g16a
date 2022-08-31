cc1f86235963391960c78981779c46ff998056fe
Uriel Buciio <urielte.cruzsa@gmail.com>
2022-08-30 19:54:18 -0500

    settear busqueda

--- 05-Frontend/Proyecto-Final/project_final/src/components/Header/Header.jsx
+++ 05-Frontend/Proyecto-Final/project_final/src/components/Header/Header.jsx
@@ -2,9 +2,9 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../assets/img/logo.png';
 
-const Header = ({getId}) => {
+const Header = ({getId, isUserLogin, authentication}) => {
   const [search, setSearch] = useState('');
-
+  console.log(isUserLogin);
   const onChange = (event) => {
     setSearch(event.target.value)
   };
@@ -19,10 +19,14 @@ const Header = ({getId}) => {
     setSearch('')
   };
 
+  const onClick = () => {
+    getId('')
+  }
+  
   return (
     <header>
       <section className="logo">
-        <Link to={'/'}>
+        <Link to={'/'} onClick={onClick}>
           <img src={logo} alt="logo"/>
           <h1 className="title">E-Commerce</h1>
         </Link>
@@ -31,9 +35,9 @@ const Header = ({getId}) => {
         <input type="search" value={search} placeholder="Buscar producto..." className="search_input" onChange={onChange}/>
         <input type="submit" value="buscar" className="search_logo" />
       </form>
-      <section className="login-container">
-        <button className="btn-login btn">Iniciar sesion</button>
-        <button className="btn-register btn">Registrar</button>
+      <section className="btn-container">
+        {isUserLogin ? <h1>Usuario</h1> : <><Link to={'/login'}><button className="btn-login btn">Iniciar sesion</button></Link>
+        <Link to={'/signup'}><button className="btn-register btn">Registrar</button></Link></>}
       </section>
     </header>
   );
