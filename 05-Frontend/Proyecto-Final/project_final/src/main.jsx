ada84bb85c85a3938ee3f43b33fe99f265ad1185
Uriel Buciio <urielte.cruzsa@gmail.com>
2022-08-30 19:52:45 -0500

    Routes

--- 05-Frontend/Proyecto-Final/project_final/src/main.jsx
+++ 05-Frontend/Proyecto-Final/project_final/src/main.jsx
@@ -1,18 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import Details from './components/Details/Details';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<App />}/>
-        <Route path='/details/:id' element={<Details />}/>
-        <Route path='*' element={<h1>404:  No found</h1>}/>
-      </Routes>
+      <App/>
     </BrowserRouter>
   </React.StrictMode>
 );
